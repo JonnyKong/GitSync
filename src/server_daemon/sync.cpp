67f50c816f9586b0b55e63b788563cfd4d5f1963
JonnyKong <jonnykong1996@gmail.com>
2019-05-11 00:47:44 -0700

    Add skeleton code for sync

--- src/server_daemon/sync.cpp
+++ src/server_daemon/sync.cpp
@@ -1,3 +1,54 @@
 #include <iostream>
+#include "boost/lexical_cast.hpp" 
 
 #include "sync.hpp"
+
+namespace ndn {
+namespace gitsync {
+
+using boost::lexical_cast; 
+
+Sync::Sync(Face& face, KeyChain& keychain, const Name& sync_prefix,
+           const UpdateCallback& update_cb)
+  : m_face(face)
+  , m_keychain(keychain)
+  , m_sync_prefix(sync_prefix)
+  , m_update_cb(update_cb)
+{
+
+}
+
+bool
+Sync::publishData(Name prefix, uint64_t t)
+{
+
+}
+
+inline static Name
+encodeVector(const VersionVector& vector)
+{
+  std::string vector_str = "";
+  for (auto e : vector)
+    vector_str += (e.first.toUri() + "-" + std::to_string(e.second) + "_");
+  return Name(vector_str);
+}
+
+inline static VersionVector
+decodeVector(const Name& vector_str)
+{
+  int start = 0;
+  VersionVector vector;
+  for (int i = 0; i < vector_str.toUri().size(); ++i) {
+    if (vector_str.toUri()[i] == '_') {
+      std::string s = vector_str.toUri().substr(start, i - start);
+      size_t sep = s.find("-");
+      Name prefix = s.substr(0, sep);
+      std::string t_str = s.substr(sep + 1);
+      vector[prefix] = lexical_cast<uint64_t>(t_str);
+    }
+  }
+  return vector;
+}
+
+} // namespace gitsync
+} // namespace ndn
--- src/server_daemon/sync.hpp
+++ src/server_daemon/sync.hpp
@@ -1,1 +1,52 @@
+// Simplified version of vectorsync
+
+#pragma once
 #include <iostream>
+
+#include <ndn-cxx/face.hpp>
+#include <ndn-cxx/security/key-chain.hpp>
+#include <ndn-cxx/util/time.hpp>
+
+
+namespace ndn {
+namespace gitsync {
+
+using VersionVector = std::map<Name, uint64_t>;
+using UpdateCallback = function<void(std::pair<Name, uint64_t>);
+
+class Sync {
+public:
+  // Need higher layers to pass a callback
+  Sync(Face& face, KeyChain& keychain, const Name& sync_prefix,
+       const UpdateCallback& update_cb);
+
+  // Publish data to a given prefix, and return whether the operation succeeded. 
+  // It may fail if the given timestamp is smaller or equal to the existing 
+  //  timestamp. Create the prefix if it doesn't exist.
+  // Need higher levels to enforce write permission.
+  bool 
+  publishData(Name prefix, uint64_t t = toUnixTimestamp(time::system_clock::now()).count());
+
+private:
+  // Encode a VersionVector object to ndn::Name, with each field separated by '_'.
+  // Example: "PrefixFoo-10_PrefixBar-20_".
+  // TODO: This encoding prevents prefixes to have "_" or "-" in them, better alternative?
+  inline static Name
+  encodeVector(const VersionVector& vector);
+
+  // Decode the version vector string to VersionVector object.
+  // TODO: This encoding prevents prefixes to have "_" or "-" in them, better alternative?
+  inline static VersionVector
+  decodeVector(const Name& vector_str);
+
+
+private:
+  Face& m_face;
+  KeyChain& m_keychain;
+  Name m_sync_prefix;
+  UpdateCallback m_update_cb;
+  VersionVector m_vector;
+};
+
+} // namespace gitsync
+} // namespace ndn
