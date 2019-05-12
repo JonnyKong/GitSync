13d8e56d39bcba415b0a7d4c0e5c450c4b3daf9a
JonnyKong <jonnykong1996@gmail.com>
2019-05-11 20:49:14 -0700

    Add header

--- src/server_daemon/logging.hpp
+++ src/server_daemon/logging.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdarg.h>     /* va_list, va_start, va_arg, va_end */
 
 void verbose(const char * fmt, ... ) {
   va_list args;  /* Used as a pointer to the next variable argument. */
