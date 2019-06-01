a690f9c4553c1de5b6d001fb0c0f625934832c7f
Xinyu Ma <bitmxy@gmail.com>
2019-06-01 11:23:58 -0700

    FileStorage

--- src/remote_helper/ndngitsync/storage.py
+++ src/remote_helper/ndngitsync/storage.py
@@ -19,18 +19,25 @@ class FileStorage(IStorage):
     def __init__(self, path_prefix: str):
         self.path = path_prefix
 
-    @staticmethod
-    def path_from_hash(hash_name: str):
-        return os.path.join("objects", hash_name[:2], hash_name[2:])
+    def path_from_hash(self, hash_name: str) -> str:
+        return os.path.join(self.path, "objects", hash_name[:2], hash_name[2:])
 
     def put(self, hash_name: str, data: bytes):
-        raise NotImplementedError
+        with open(self.path_from_hash(hash_name), "wb") as f:
+            f.write(data)
 
     def get(self, hash_name: str) -> bytes:
-        raise NotImplementedError
+        with open(self.path_from_hash(hash_name), "rb") as f:
+            raw_data = f.read()
+        return raw_data
 
     def exists(self, hash_name: str) -> bool:
-        raise NotImplementedError
+        return os.path.exists(self.path_from_hash(hash_name))
 
     def remove(self, hash_name: str) -> bool:
-        raise NotImplementedError
+        try:
+            os.remove(self.path_from_hash(hash_name))
+            return True
+        except OSError:
+            return False
+
