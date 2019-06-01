cff9007ef0e167c8507b2cc268140c2b8cd6ea08
JonnyKong <jonnykong1996@gmail.com>
2019-06-01 16:48:41 -0700

    Storage: Add key iterator func

--- src/remote_helper/ndngitsync/storage.py
+++ src/remote_helper/ndngitsync/storage.py
@@ -46,7 +46,7 @@ class FileStorage(IStorage):
             return False
 
 class DBStorage(IStorage):
-    def __init__(self, db: str="gitsync", collection: str="objects"):
+    def __init__(self, db: str, collection: str):
         """
         Init DB with unique index on hash
         """
@@ -57,9 +57,9 @@ class DBStorage(IStorage):
         client = MongoClient(self._uri)
         c_db = client[self._db]
         c_collection = c_db[self._collection]
-        c_collection.create_index('hash', unique=True)
+        c_collection.create_index('key', unique=True)
     
-    def put(self, hash_name: str, data: bytes):
+    def put(self, key: str, value: bytes):
         """
         Insert document into MongoDB, overwrite if already exists.
         """
@@ -67,44 +67,53 @@ class DBStorage(IStorage):
         c_db = client[self._db]
         c_collection = c_db[self._collection]
         document = {
-            "hash": hash_name,
-            "data": data
+            "key": key,
+            "value": value
         }
         try:
             c_collection.insert_one(document).inserted_id
         except pymongo.errors.DuplicateKeyError:
-            c_collection.update_one({"hash": hash_name}, {"$set": {"data": data}})
+            c_collection.update_one({"key": key}, {"$set": {"value": value}})
     
-    def get(self, hash_name: str) -> bytes:
+    def get(self, value: str) -> bytes:
         """
         Get document from MongoDB
         """
         client = MongoClient(self._uri)
         c_db = client[self._db]
         c_collection = c_db[self._collection]
-        ret = c_collection.find_one({"hash": hash_name})
+        ret = c_collection.find_one({"value": value})
         if ret:
-            return ret["data"]
+            return ret["value"]
         else:
             return None
 
-    def exists(self, hash_name: str) -> bool:
+    def exists(self, key: str) -> bool:
         """
         Return whether document exists
         """
         client = MongoClient(self._uri)
         c_db = client[self._db]
         c_collection = c_db[self._collection]
-        if c_collection.find_one({"hash": hash_name}):
+        if c_collection.find_one({"key": key}):
             return True
         else:
             return False
     
-    def remove(self, hash_name: str) -> bool:
+    def remove(self, key: str) -> bool:
         """
         Return whether removal is successful
         """
         client = MongoClient(self._uri)
         c_db = client[self._db]
         c_collection = c_db[self._collection]        
-        return c_collection.delete_one({"hash": hash_name}).deleted_count > 0
+        return c_collection.delete_one({"key": key}).deleted_count > 0
+
+    def keys(self):
+        """
+        Return a set of "primary" keys
+        """
+        client = MongoClient(self._uri)
+        c_db = client[self._db]
+        c_collection = c_db[self._collection]
+        return (doc["key"] for doc in c_collection.find())
